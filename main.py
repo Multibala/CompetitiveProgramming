ac6f1bffe05bb9ddd31fff0e4e21f7786996f89f
Multibala <qmultibala@gmail.com>
2022-01-18 23:01:38 +0600

    problem solution

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ MI = lambda:map(int,SI().split())
 II = lambda:int(SI())
 LI = lambda:list(MI())
 lcm = lambda x,y:abs(x*y)//math.gcd(x,y)
-
+filledArr = lambda size,value: [value for _ in range(size)]
 
 class Solution:
     def __init__(self) -> None:
