1ecc87fd968e9469cc429ea1efde0045f66836ee
Multibala <qmultibala@gmail.com>
2022-01-18 16:49:17 +0600

    adding main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import sys
+import math
+
+SI = lambda:sys.stdin.readline().rstrip("\r\n")
+MI = lambda:map(int,SI().split())
+II = lambda:int(SI())
+LI = lambda:list(MI())
+lcm = lambda x,y:abs(x*y)//math.gcd(x,y)
+
+
+class Solution:
+    def __init__(self) -> None:
+        pass
+if __name__ == '__main__':
+    Solution()
+
