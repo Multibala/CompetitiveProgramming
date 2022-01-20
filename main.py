b0167a0c9f8341f2b62937029e8fd5344507c4e8
Multibala <qmultibala@gmail.com>
2022-01-20 18:30:51 +0600

    rebuging main.py

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import sys
 import math
 
-SI = lambda:sys.stdin.readline().rstrip("\r\n")
-MI = lambda:map(int,SI().split())
-II = lambda:int(SI())
-LI = lambda:list(MI())
+SI = lambda: sys.stdin.readline().rstrip("\r\n")
+MI = lambda: map(int,SI().split())
+II = lambda: int(SI())
+LI = lambda: list(MI())
 lcm = lambda x,y:abs(x*y)//math.gcd(x,y)
-filledArr = lambda size,value: [value for _ in range(size)]
+FA = lambda size,value: [value for _ in range(size)] # get filled array
 
-class Solution:
-    def __init__(self) -> None:
-        pass
-if __name__ == '__main__':
-    Solution()
+
+
+
+
+
 
