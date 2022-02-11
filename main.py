e1c6eab2f9542e9f003ba3a5e875eab2d483326e
Multibala <qmultibala@gmail.com>
2022-02-11 15:30:17 +0600

    'update'

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
-
+from re import L
 import sys
 import math
+from collections import deque
+import random
+ 
+# Multi 
+ 
+out  = lambda x,end='\n' : sys.stdout.write(str(x)+end)
+SI   = lambda: sys.stdin.readline().rstrip("\r\n")
+MI   = lambda: map(int,SI().split())
+II   = lambda: int(SI())
+LI   = lambda: list(MI())
+lcm  = lambda x,y:abs(x * y) // math.gcd(x,y)
+FA   = lambda size,value: [value for _ in range(size)]
+FCT  = lambda x:eval('*'.join([str(i) for i in range(1,x+1)])) if x>1 else 1
+INF  = float("inf")
 
-SI = lambda: sys.stdin.readline().rstrip("\r\n")
-MI = lambda: map(int,SI().split())
-II = lambda: int(SI())
-LI = lambda: list(MI())
-lcm = lambda x,y:abs(x*y)//math.gcd(x,y)
-FA = lambda size,value: [value for _ in range(size)] # get filled array
 
 
+for _ in range(II()):
+    n = II()
+    temp = FA(5,set())
+    for i in range(n):
+        l = LI()
+        for j in range(5):
+            if l[j] == 1:
+                temp[j].add(j)
 
+    temp.sort()
 
+    print(['NO','YES'][temp[-1]>=n//2 and temp[-2] >= n//2])
+    
 
-
+    
